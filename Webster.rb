d73a5d9ba9835c647ade972078efa752c7c47e99
Manav Sharma <m-13@users.noreply.github.com>
2015-11-25 15:37:30 +0530

    Removed unnecessary newline

--- Webster.rb
+++ Webster.rb
@@ -4,10 +4,8 @@ require_relative 'HttpResponse'
 
 server = TCPServer.new("localhost", 13131)
 
-
 while true do
 	Thread.start(server.accept) do |client|
-
 		request = HTTPRequest.new(client.gets)	
 		puts request.requestString
 		client.puts request.process.response
